184e5e0a2b246c5b6c186ae9709b68a267f529fb
gabs-v <gabyvazquez8297@gmail.com>
2022-03-20 14:33:11 -0700

    first commit

---
+++ basic_translator.py
@@ -0,0 +1,12 @@
+#translating a basic translator with our made-up language 
+
+def translate (phrase):
+    translation = ""
+    for letter in phrase:
+        if letter in "AEIOUaeiou" :
+            translation = translation + "g"
+        else:
+            translation = translation + letter 
+    return translation
+
+print(translate(input("Enter a phrase: ")))
